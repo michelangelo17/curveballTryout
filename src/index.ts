152debd774b268a9792d13f72d88c67723282df5
Michelangelo Markus <michelangelo.markus@gmail.com>
2020-06-26 10:47:32 -0700

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import app from './app';
+
+console.log('⚾ Curveball v%s', require('@curveball/core/package.json').version);
+
+// The HTTP port can be overridden via the 'PORT' environment variable.
+const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 8500;
+app.listen(port);
+
+console.log('Listening on port %i', port);
